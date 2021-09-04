3119b094eaf2dc8629cfa432e23f6e19b6895fc6
GiowGiow <giofmartinelli@gmail.com>
2021-09-04 03:39:06 -0300

    Updating notebooks and generation

---
+++ code/generate_network.py
@@ -0,0 +1,136 @@
+import networkx as nx
+import pandas as pd
+import numpy as np
+import collections
+
+from tqdm import tqdm
+from networkx.drawing.nx_pylab import draw_networkx
+
+
+def generate_network(list_of_songs, similarity_matrix, similarity_threshold=0.80):
+    """ Generate a disruption network based on: 
+    If a song has a similarity with another over the threshold than an edge is made to connect both of them.
+
+    Args:
+        1. list of songs (in the same order as the similarity matrix)
+        2. A similarity matrix (ordered by release) so we know that the next song i + 1 is a song released after i
+        3. A similarity threshold that will determine if there is an edge between nodes
+    Returns:
+        A network as a networkx.DiGraph Object
+    """
+    slice_index = 0
+    G = nx.DiGraph()
+
+    for i in tqdm(range(len(list_of_songs))):
+        edge_count = 0
+        
+        G.add_node(i)
+        
+        for j in range(i + 1, len(list_of_songs)):
+            # If there is a high similarity, create an edge between the nodes
+            if similarity_matrix[i][j] > similarity_threshold:
+                G.add_edge(j, i)
+                edge_count += 1
+        
+        # If this node does not have a similarity with any other node, then remove the node
+        if edge_count < 1:
+            G.remove_node(i)
+
+    return G
+
+def get_disruption_index_for_nodes(list_of_songs, graph):
+    """ Compute the actual disruption indexes for the graph based on the nodes(songs) and its
+    connections (if its influenced or if it influenced another song) """
+    disruption_info = {}
+
+    for i in tqdm(range(len(list_of_songs))):    
+        if graph.has_node(i):
+            songs_after = range(i + 1, len(list_of_songs))
+            song_influences = [edge[1] for edge in graph.edges(i) if edge[1] != i]
+
+            ni = 0
+            nj = 0
+            nk = 0
+            
+            for song_after in songs_after:
+                consolidating_influence = False
+                if graph.has_edge(song_after , i):
+                
+                    for influence in song_influences:
+                        if graph.has_edge(song_after, influence):
+                            consolidating_influence = True
+                            break
+                
+                    if consolidating_influence:
+                        nj += 1
+                    else:
+                        ni += 1
+                
+                else:
+                    for influence in song_influences:
+                        if graph.has_edge(song_after, influence):
+                            nk += 1
+    
+            disruption_info[list_of_songs.iloc[i]['id']] = [ni, nj, nk, float((ni-nj)) / float((ni+nj+nk))] if (ni + nj + nk) > 0 else [ni, nj, nk, 0]
+    
+    return disruption_info
+
+
+from pathlib import Path
+
+DATASET_PATH = Path("./dataset")
+
+def load_npy(file_path):
+    print(f"Loading: {file_path} ...")
+    return np.load(file_path)
+
+def load_features_from_file(file_name, feature_type):
+    if feature_type == "mfcc":
+        return load_npy(DATASET_PATH / "input" / "feature_vectors" / "mfcc" / file_name)
+    elif feature_type == "transfer_learning":
+        return load_npy(DATASET_PATH / "input" / "feature_vectors" / "transfer_learning" / file_name)
+    else:
+        raise TypeError("This feature type is not supported")
+
+def load_similarity_matrix(file_name, feature_type):
+    if feature_type == "mfcc":
+        return load_npy(DATASET_PATH / "input" / "similarity_matrices" / "mfcc" / file_name)
+    elif feature_type == "transfer_learning":
+        return load_npy(DATASET_PATH / "input" / "similarity_matrices" / "transfer_learning" / file_name)
+    else:
+        raise TypeError("This feature type is not supported")
+
+def load_dataframe(dataframe_file):
+    return pd.read_csv(DATASET_PATH / "input" / "csvs" / dataframe_file)
+
+feat_type = "transfer_learning"
+datset_size = 30000
+gamma = 0.1
+
+DF_PATH = f"sorted_song_info_{datset_size}.csv"
+FEATS_PATH = f"{feat_type}_feature_vector_{datset_size}_samples.npy"
+SIMILARITY_MATRIX_PATH = f"{feat_type}_{datset_size}_samples_{gamma}_gamma.npy"
+
+dataframe = load_dataframe(DF_PATH)
+features = load_features_from_file(FEATS_PATH, feat_type)
+similarity_matrix = load_similarity_matrix(SIMILARITY_MATRIX_PATH, feat_type)
+
+graph = generate_network(dataframe, similarity_matrix)
+
+disruption_index = get_disruption_index_for_nodes(dataframe, graph)
+
+nx.write_gexf(graph, DATASET_PATH / "output" / "graphs" / f"{feat_type}_{len(disruption_index)}_{gamma}.gexf")
+
+import pickle
+
+# Store data (serialize)
+with open(DATASET_PATH / "output" / "disruption_index" / f'{feat_type}_{len(disruption_index)}_{gamma}.pickle', 'wb') as handle:
+    pickle.dump(disruption_index, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+disruption_index_df = pd.DataFrame(disruption_index).T
+disruption_index_df.reset_index(inplace=True)
+disruption_index_df.columns = ['id', 'ni', 'nj', 'nk', 'disruption']
+
+song_info_with_disruption = pd.merge(disruption_index_df, dataframe, on='id')
+
+song_info_with_disruption.to_csv(DATASET_PATH / "output" / "csv_with_disruption" / f"song_info_with_disruption_{len(song_info_with_disruption)}_feat_{feat_type}_gamma_{gamma}.csv", index=False)
--- code/src/utils.py
+++ code/src/utils.py
@@ -10,7 +10,7 @@ def plot_genre_distribution(df, most_popular=15):
     
     # That way we can count the occurrences of all genres in the dataframe
     genres_series = pd.Series(genres)
-    print(f"Number of unique genres in the dataset: {genres.series.nunique()}")
+    print(f"Number of unique genres in the dataset: {genres_series.nunique()}")
     
     genres_count = genres_series.value_counts()
     ax = genres_count[:most_popular].plot.bar(title="Distribuição de Gêneros no dataset")
@@ -43,3 +43,44 @@ def find_empty_songs_export_dataset(df, similarity_matrix):
     
     print(f"Exporting dataset of size {get_df_size(df)}")
     df.to_csv(f"song_info_dataset_{get_df_size(df)}_entries_filtered.csv", index=False)
+
+
+from math import ceil
+
+"""
+# Helper functions to help analyze/filter the dataset 
+# Exploring a bit with the information we got
+## Showing some songs with high similarity
+
+### When using gamma = 0.01, only masters and remasters of the same songs show as similar
+# get_songs_list(dataframe, similarity_matrix, 0.8)
+
+beatles_df = dataframe.query("artist == 'The Beatles'")
+print(len(beatles_df))
+beatles_feature_vector = get_feature_vector(beatles_df)
+beatles_similarity_matrix = rbf_kernel(beatles_feature_vector, gamma=0.001)
+np.fill_diagonal(beatles_similarity_matrix, 0)
+get_songs_list(beatles_df, beatles_similarity_matrix, 0.7)
+
+"""
+
+def show_similarity(df, similarity_matrix, song_index, song_two_index):
+    """ Shows the similarity of two entries of a similarity matrix """
+    print()
+    print(df.iloc[song_index])
+    print(df.iloc[song_two_index])
+    print()
+    similarity = similarity_matrix[song_index, song_two_index]
+    print(f"Similarity: {similarity}")
+    
+def get_songs_list(df, similarity_matrix, similarity_threshold, limit=10):
+    """ Get the list of song similarities for a similarity threshold """
+    rows, columns = np.where(similarity_matrix > similarity_threshold)
+    print(len(similarity_matrix[rows, columns]))
+    songs_list = np.array((rows, columns)).T
+    songs_list = songs_list[:ceil(len(songs_list)/2)]
+    for song in songs_list[:limit]:
+        show_similarity(df, similarity_matrix, song[0], song[1])
+
+
+
